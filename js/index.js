990d6f49a6d98ffdf2558b2cecd8451b743960d2
intellectmind-dot <intellectmind999@gmail.com>
2025-02-17 23:46:56 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'My Shawty Sweetie 😉'.split('')
+const text = 'For my Shawty Sweetie'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
